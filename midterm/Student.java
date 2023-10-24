93eff9ea4f534790e6528a43d340ec1fedd09277
Fallen032 <94450839+Fallen032@users.noreply.github.com>
2023-10-24 05:12:48 +0100

    Add files via upload

---
+++ midterm/Student.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class Student {
+   String first_name;
+   String middle_name;
+   String last_name;
+   String suffix;
+   String full_name;
+   
+   public void setfirstName(String Name){
+      this.first_name = Name;
+   }
+   public void setmiddleName(String Middle){
+      this.middle_name = Middle;
+   }
+   public void setlastName(String Last){
+      this.last_name = Last;
+   }
+   public void setSuffix(String Suffix){
+      this.suffix = Suffix;
+   }
+   public String getfirstName(){
+      return first_name;
+   }
+   public String getmiddleName(){
+      return middle_name;
+   }
+   public String getlastName(){
+      return last_name;
+   }
+   public String getSuffix(){
+      return suffix;
+   }
+   public String fullName(){
+      return first_name + " " + middle_name + " " + last_name + " " + suffix;
+   }
+   
+   public static void main(String[] args) {
+    Student info = new Student();
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter first name: ");
+    String first = input.nextLine();
+    info.setfirstName(first);
+    
+    System.out.print("Enter middle name: ");
+    String middle = input.nextLine();
+    info.setmiddleName(middle);
+    
+    System.out.print("Enter last name: ");
+    String last = input.nextLine();
+    info.setlastName(last);
+    
+    System.out.print("Enter suffix: ");
+    String suffix = input.nextLine();
+    info.setSuffix(suffix);
+    
+    // Display full name
+    System.out.println("Full Name: " + info.fullName());
+ }
+}
